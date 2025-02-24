2ec49ea050842e3db8e44a927f60f5d246c0d500
araneet <martinezcruzaj@gmail.com>
2025-02-24 13:30:51 -0400

    Fixing type error

--- app/page.tsx
+++ app/page.tsx
@@ -295,7 +295,7 @@ export default function Home() {
 interface SkillCardProps {
   title: string
   skills: string[]
-  icon: SVGRectElement
+  icon: React.ReactNode
 }
 // Helper Components
 function SkillCard({ title, skills, icon }: SkillCardProps) {
