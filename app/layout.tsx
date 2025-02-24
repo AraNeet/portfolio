ee9cc6031b2eac8543af8cdddc48267bb8122f0d
araneet <martinezcruzaj@gmail.com>
2025-02-24 13:17:51 -0400

    removing unused imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,7 @@
 import type React from "react"
 import type { Metadata } from "next"
-import { Sigmar, Lato, Coustard } from "next/font/google"
+import { Coustard } from "next/font/google"
 import "./globals.css"
-
-const sigmar = Sigmar({
-  weight: "400",
-  subsets: ["latin"],
-  display: "swap",
-})
-
-const lato = Lato({
-  weight: "400",
-  subsets: ["latin"],
-  display: "swap",
-})
 
 const coustard = Coustard({
   weight: "400",
