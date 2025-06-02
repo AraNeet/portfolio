dc6f1ff0a5b7325e95062078f86fe9f70f41eb22
araneet <martinezcruzaj@gmail.com>
2025-06-02 12:12:30 -0400

    Lint errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { motion } from "framer-motion"
-import { Code2Icon, Briefcase, Mail, Phone, Languages, Boxes, Paperclip } from "lucide-react"
+import { Code2Icon, Briefcase, Mail, Phone, Boxes, Paperclip } from "lucide-react"
 import { Card } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
 import Link from "next/link"
